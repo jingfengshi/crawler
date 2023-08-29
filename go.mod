44a77742f88ed53ededef22f946ba6cc310ce0b7
jasperjing <240023810@qq.com>
2023-08-29 19:35:23 +0800

    转码

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jasperjing/crawler
 
 go 1.18
+
+require (
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	transform "golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
 )
@@ -22,7 +26,7 @@ func main() {
 		return
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := Fetch(url)
 
 	if err != nil {
 		fmt.Println("read content failed:%v", err)
@@ -30,4 +34,31 @@ func main() {
 	}
 
 	fmt.Println("body:", string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
