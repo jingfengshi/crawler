f7dadc7e9f30ebba7319d0f49bed9f1234df135c
jasperjing <240023810@qq.com>
2023-08-30 16:03:32 +0800

    schdule

--- main.go
+++ main.go
@@ -3,53 +3,49 @@ package main
 import (
 	"fmt"
 	"github.com/jasperjing/crawler/collect"
+	"github.com/jasperjing/crawler/engine"
 	"github.com/jasperjing/crawler/log"
 	"github.com/jasperjing/crawler/parse/doubangroup"
-	"go.uber.org/zap"
+	"github.com/jasperjing/crawler/proxy"
 	"go.uber.org/zap/zapcore"
 	"time"
 )
 
 func main() {
+
+	// log
 	plugin := log.NewStdoutPlugin(zapcore.InfoLevel)
 	logger := log.NewLogger(plugin)
 	logger.Info("log init end")
 
-	// douban cookie
-	cookie := "bid=-UXUw--yL5g; dbcl2=\"214281202:q0BBm9YC2Yg\"; __yadk_uid=jigAbrEOKiwgbAaLUt0G3yPsvehXcvrs; push_noty_num=0; push_doumail_num=0; __utmz=30149280.1665849857.1.1.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmv=30149280.21428; ck=SAvm; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1665925405%2C%22https%3A%2F%2Faccounts.douban.com%2F%22%5D; _pk_ses.100001.8cb4=*; __utma=30149280.2072705865.1665849857.1665849857.1665925407.2; __utmc=30149280; __utmt=1; __utmb=30149280.23.5.1665925419338; _pk_id.100001.8cb4=fc1581490bf2b70c.1665849856.2.1665925421.1665849856."
+	// proxy
+	proxyURLs := []string{"http://127.0.0.1:8888", "http://127.0.0.1:8888"}
+	p, err := proxy.RoundRobinProxySwitcher(proxyURLs...)
+	if err != nil {
+		logger.Error("RoundRobinProxySwitcher failed")
+	}
 
-	var worklist []*collect.Request
-
-	for i := 25; i <= 100; i += 25 {
+	var seeds []*collect.Request
+	for i := 0; i <= 0; i += 25 {
 		str := fmt.Sprintf("https://www.douban.com/group/szsh/discussion?start=%d", i)
-		worklist = append(worklist, &collect.Request{
+		seeds = append(seeds, &collect.Request{
 			Url:       str,
-			Cookie:    cookie,
+			Cookie:    "bid=-UXUw--yL5g; dbcl2=\"214281202:q0BBm9YC2Yg\"; __yadk_uid=jigAbrEOKiwgbAaLUt0G3yPsvehXcvrs; push_noty_num=0; push_doumail_num=0; __utmz=30149280.1665849857.1.1.utmcsr=accounts.douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmv=30149280.21428; ck=SAvm; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1665925405%2C%22https%3A%2F%2Faccounts.douban.com%2F%22%5D; _pk_ses.100001.8cb4=*; __utma=30149280.2072705865.1665849857.1665849857.1665925407.2; __utmc=30149280; __utmt=1; __utmb=30149280.23.5.1665925419338; _pk_id.100001.8cb4=fc1581490bf2b70c.1665849856.2.1665925421.1665849856.",
 			ParseFunc: doubangroup.ParseURL,
 		})
 	}
 
-	var f collect.Fetcher = collect.BrowserFetch{
+	var f collect.Fetcher = &collect.BrowserFetch{
+		Proxy:   p,
 		Timeout: 3000 * time.Millisecond,
 	}
 
-	for len(worklist) > 0 {
-		items := worklist
-		worklist = nil
-		for _, item := range items {
-			body, err := f.Get(item)
-			time.Sleep(1 * time.Second)
-			if err != nil {
-				logger.Error("read content failed", zap.Error(err))
-				continue
-			}
-			res := item.ParseFunc(body, item)
-			for _, item := range res.Items {
-				logger.Info("result", zap.String("item", item.(string)))
-			}
-			worklist = append(worklist, res.Requests...)
-		}
-
+	s := engine.ScheduleEngine{
+		WorkCount: 5,
+		Fetcher:   f,
+		Logger:    logger,
+		Seeds:     seeds,
 	}
+	s.Run()
 
 }
